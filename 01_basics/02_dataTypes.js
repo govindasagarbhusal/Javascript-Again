879842adf9617ed38c1b8ab447527925edd959a5
Govinda Sagar Bhusal <govindasagarbhusal@gmail.com>
2024-12-26 20:06:32 +0545

    Data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"Use strict"// treat all JS code as newer version
+
+// alert("Hello World"); // pop up message in the browser
+
+let name = "Govinda";
+let age = 20;
+
+
+/* 
+    Data Types:
+    1. Number
+    2. String = ""
+    3. Boolean = Yes or No
+    4. Undefined
+    5. Null= empty
+    6. Object
+    7. Symbol = unique
+    8. BigInt
+*/
+
+console.log(typeof age);
+
+// type of null = object
