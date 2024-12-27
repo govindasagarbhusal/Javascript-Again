4a1669457e87cda46e7b3214f39030929e2c78b1
Govinda Sagar Bhusal <govindasagarbhusal@gmail.com>
2024-12-27 19:50:11 +0545

    Conversion operations advanced

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -29,6 +29,49 @@
 // "" => False
 // "Govinda" => True
 
-let someNumber = 33;
-let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33;
+// let stringNumber = String(someNumber);
+// console.log(typeof stringNumber);
+
+// ********************************Operations***********************************************
+
+// let value = 3
+// let negValue = -value
+// console.log(value);
+// console.log(negValue);
+
+// console.log(2 + 2); => addition
+// console.log(2 - 2); => subtraction
+// console.log(2 * 2); => multiplication
+// console.log(2 ** 2); => 2 to the power 2
+// console.log(2 ** 3); => 2 to the power three
+// console.log(2 / 3); => divide
+// console.log(2 % 3); => remainder 
+
+// let str1 = "Hello"
+// let str2 = " Govinda"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);  => 12
+// console.log(1 + "2"); => 12
+// console.log("1" + 2 + 2); => 122
+// console.log(1 + 2 + "2"); => 32
+
+// console.log(true);
+// console.log(+true); => 1
+
+// console.log(+""); => 0 || Never do this kind of code
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2+2
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+// let gameCounter = 100;
+// gameCounter++;
+// ++gameCounter;
+// console.log(gameCounter);
---
+++ 01_basics/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+
+// let str2 = " Govinda"
+
+// let str3 = str1 + str2
+// console.log(str3);
