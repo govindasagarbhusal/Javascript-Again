02090c1f1ffeefd534f0f5e1d73eb59b569e2329
Govinda Sagar Bhusal <govindasagarbhusal@gmail.com>
2025-01-01 21:23:50 +0545

    string

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -5,5 +5,20 @@ const repoCount = 7
 
 // console.log(`Hello my name is ${name}, I have ${repoCount} repositories on github`);
 
-const gameName = new String('govindasb')
-// console.log(gameName);
+const gameName = new String('govinda-sb')
+// console.log(gameName[0]); => g
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('n'));
+
+const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+const newStringOne = "    govinda    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
